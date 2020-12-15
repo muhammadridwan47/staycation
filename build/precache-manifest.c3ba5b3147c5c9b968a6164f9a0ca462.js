fcf8737251b923e17079daad1a1928b6dae20937
muhammadridwan47 <rolilaz47@gmail.com>
2020-12-15 16:19:16 +0700

    set up url

---
+++ build/precache-manifest.c3ba5b3147c5c9b968a6164f9a0ca462.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e992f4a3f1ad94ea62581c4fc68faa76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "541b7e725bc0d3850751",
+    "url": "/static/css/2.7196f3b3.chunk.css"
+  },
+  {
+    "revision": "4e8bb2a2040d333fe661",
+    "url": "/static/css/main.4365684e.chunk.css"
+  },
+  {
+    "revision": "541b7e725bc0d3850751",
+    "url": "/static/js/2.3036e72b.chunk.js"
+  },
+  {
+    "revision": "e2f8921074e6a11cc3d28a7268d4ec3b",
+    "url": "/static/js/2.3036e72b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8bb2a2040d333fe661",
+    "url": "/static/js/main.eef8cd10.chunk.js"
+  },
+  {
+    "revision": "f6f913e839527fe13eca",
+    "url": "/static/js/runtime-main.dd9a29b7.js"
+  },
+  {
+    "revision": "3366f1365b8130f7b2eff3d2b3bba490",
+    "url": "/static/media/completed.3366f136.jpg"
+  },
+  {
+    "revision": "67e25a39beb992b5476840cc2ce5aa21",
+    "url": "/static/media/icon-calendar.67e25a39.svg"
+  },
+  {
+    "revision": "eacd8f56422fa4e01a3a7b6005955918",
+    "url": "/static/media/img-hero.eacd8f56.jpg"
+  },
+  {
+    "revision": "11a62c712d17eaee33a02927afd01988",
+    "url": "/static/media/star.11a62c71.svg"
+  }
+]);
